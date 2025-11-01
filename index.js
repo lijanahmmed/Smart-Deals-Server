d79980c6fc392b1974099bc44691384b8b8513ba
lijanahmmed <lijanahmmed@gmail.com>
2025-11-01 19:20:54 +0600

    create simple deals server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://simpleDBUser:4YB0cNuMWV8m5loq@cluster0.3kkgkzf.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Simple Deals is running");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Simple Deals on port ${port}`);
+});
