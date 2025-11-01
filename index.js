dbb715871790d896d5719237652c0ff9643b6ac4
lijanahmmed <lijanahmmed@gmail.com>
2025-11-01 19:22:19 +0600

    added post sever

--- index.js
+++ index.js
@@ -22,7 +22,28 @@ app.get("/", (req, res) => {
   res.send("Simple Deals is running");
 });
 
+async function run() {
+  try {
+    await client.connect();
 
+    const db = client.db("smart_db");
+    const productCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Simple Deals on port ${port}`);
