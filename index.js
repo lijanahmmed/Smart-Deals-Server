7f2e5fcd860d65dd4d77ccd30cb57b3359471332
lijanahmmed <lijanahmmed@gmail.com>
2025-11-01 19:25:43 +0600

    get specific one product functionalities

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ async function run() {
       res.send(result);
     });
 
-    
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
